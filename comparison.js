d55b7e090daebd9668e2349133d5e53414bde330
Ashahab007 <ashahab007@gmail.com>
2025-02-06 03:09:57 +0600

    my first

--- comparison.js
+++ comparison.js
@@ -23,3 +23,23 @@ if ((salary > 25000 || hasCar) && bcsCader) {
 } else {
   console.log("I will not marry him.");
 }
+
+// -------------------nested ternary operator--------------------
+let price = 250;
+// const isLeader = true;
+const isLeader = false;
+
+price = isLeader
+  ? 0
+  : price <= 250
+  ? price - price * 0.05
+  : price - price * 0.1;
+console.log(Math.ceil(price));
+
+let inputVal = !null;
+/* if (!inputVal) {
+  console.log(inputVal);
+} */
+console.log(inputVal);
+
+// Double Bang (!!)
